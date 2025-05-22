d95c7de8197b5418d8a8fcb54538b0638cc22b11
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-22 17:17:41 +0700

    update

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,4 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { prettyJSON } from "hono/pretty-json";
 import { apiReference } from "@scalar/hono-api-reference";
 import { AuthController } from "./http/controllers/auth.controller";
 import { CryptoController } from "./http/controllers/crypto.controller";
