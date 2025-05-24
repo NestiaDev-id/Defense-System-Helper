b1f039d9b0c16f6b012227c11b109286bd236173
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-25 06:49:20 +0700

    update auth Interface python

--- app/http/interfaces/auth.interface.ts
+++ app/http/interfaces/auth.interface.ts
@@ -28,3 +28,36 @@ export interface VerifyPasswordRequest {
 export interface VerifyPasswordResponse {
   valid: boolean;
 }
+
+// Python responses
+export interface PythonRegisterSuccessResponse {
+  message: string;
+}
+
+// Definisikan tipe respons yang diharapkan dari Python untuk setiap endpoint
+export interface PythonMessageResponse {
+  // Umumnya untuk register, dll.
+  message: string;
+  [key: string]: any;
+}
+
+export interface PythonErrorResponse {
+  detail: string | { msg: string; type: string }[] | any;
+}
+
+export interface PythonLoginSuccessResponse {
+  message: string;
+  username: string;
+}
+
+export interface PythonHashPasswordResponse {
+  hash: string;
+}
+
+export interface PythonVerifyPasswordResponse {
+  valid: boolean;
+}
+
+export interface PythonRefreshTokenSuccessResponse {
+  access_token: string;
+}
