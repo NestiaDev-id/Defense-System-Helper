c1249058a9ab1f2baae29871a1bd9a24e4cf67bb
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-25 09:07:27 +0700

    add name validation

--- app/utils/validators.ts
+++ app/utils/validators.ts
@@ -1,5 +1,6 @@
 // Regex Definitions
 export const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+export const nameRegex = /^[a-zA-Z\s]+$/;
 export const passwordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/;
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -84,6 +85,10 @@ export function validateExactLength(
   return { isValid: true };
 }
 
+export const validateName = makeValidator(
+  nameRegex,
+  "Name must contain only letters and spaces."
+);
 export const validateEmail = makeValidator(emailRegex, "Invalid email format.");
 export const validatePhone = makeValidator(
   phoneRegex,
